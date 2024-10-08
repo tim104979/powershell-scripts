4dc55d1751794e6d0297dd2391c504452eb14451
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-08 16:40:45 +0200

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,6 +1,3 @@
-# Create a new user
-New-LocalUser -Name "hacked" -Description "New user with admin rights" -Password (ConvertTo-SecureString "12345" -AsPlainText -Force)
-
 # Add the user to the Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member "hacked"
 
