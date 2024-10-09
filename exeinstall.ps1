eaea0cc246338f8c784d89322ab7f8cb1bac9585
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-09 09:09:35 +0200

    Update exeinstall.ps1

--- exeinstall.ps1
+++ exeinstall.ps1
@@ -1,40 +1,59 @@
-# Define the URL of the Ninite executable file
-$FileUri = "https://ninite.com/chrome-winrar/ninite.exe"
+# Define the URLs of the installation files
+$WinRarUrl = "https://www.win-rar.com/fileadmin/winrar-versions/winrar-x64-601.exe"
+$ChromeUrl = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
 
-# Define the destination path for the file
-$Destination = "C:\Ninite.exe"
+# Define the destination paths for the files
+$WinRarDestination = "C:\WinRAR.exe"
+$ChromeDestination = "C:\Chrome.exe"
 
-# Download the file using BITS
-$bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
+# Download the files using BITS
+$WinRarJob = Start-BitsTransfer $WinRarUrl -Destination $WinRarDestination
+$ChromeJob = Start-BitsTransfer $ChromeUrl -Destination $ChromeDestination
 
-# Wait for the download to complete
-while ($bitsJobObj.JobState -eq "Transferring") {
+# Wait for the downloads to complete
+while ($WinRarJob.JobState -eq "Transferring" -or $ChromeJob.JobState -eq "Transferring") {
     Start-Sleep -Milliseconds 100
 }
 
-# Check the job state
-switch ($bitsJobObj.JobState) {
-
+# Check the job states
+switch ($WinRarJob.JobState) {
     'Transferred' {
         # Complete the BITS transfer
-        Complete-BitsTransfer -BitsJob $bitsJobObj
+        Complete-BitsTransfer -BitsJob $WinRarJob
 
-        # Define the arguments for the Ninite installer
-        $exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
-
-        # Force 32-bit execution on 64-bit systems
+        # Install WinRAR
+        $WinRarArgs = '/S'
         $workingDir = if ([Environment]::Is64BitOperatingSystem) { "C:\Windows\SysWOW64" } else { $env:SystemRoot }
-
-        # Run the Ninite installer
-        Start-Process -Wait -WorkingDirectory $workingDir -FilePath $Destination -ArgumentList $exeArgs
+        Start-Process -Wait -WorkingDirectory $workingDir -FilePath $WinRarDestination -ArgumentList $WinRarArgs
 
         # Remove the file after installation
-        Remove-Item -Path $Destination -Force
+        Remove-Item -Path $WinRarDestination -Force
         break
     }
 
     'Error' {
         # Throw an error if the download fails
-        throw 'Error downloading'
+        throw 'Error downloading WinRAR'
+    }
+}
+
+switch ($ChromeJob.JobState) {
+    'Transferred' {
+        # Complete the BITS transfer
+        Complete-BitsTransfer -BitsJob $ChromeJob
+
+        # Install Chrome
+        $ChromeArgs = '/silent /install'
+        $workingDir = if ([Environment]::Is64BitOperatingSystem) { "C:\Windows\SysWOW64" } else { $env:SystemRoot }
+        Start-Process -Wait -WorkingDirectory $workingDir -FilePath $ChromeDestination -ArgumentList $ChromeArgs
+
+        # Remove the file after installation
+        Remove-Item -Path $ChromeDestination -Force
+        break
+    }
+
+    'Error' {
+        # Throw an error if the download fails
+        throw 'Error downloading Chrome'
     }
 }
