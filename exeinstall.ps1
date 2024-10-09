5a13e406f351b589ab9e19719dc575dc7154a1cb
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-09 08:53:59 +0200

    Update exeinstall.ps1

--- exeinstall.ps1
+++ exeinstall.ps1
@@ -1,11 +1,20 @@
-# Define the URL of the executable file
-$url = "https://ninite.com/chrome-winrar/ninite.exe"
+$FileUri = "https://ninite.com/chrome-winrar/ninite.exe"
+$Destination = "C:/File.exe"
 
-# Define the path where the file will be saved
-$filePath = "C:\File.exe"
+$bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
-# Download the file using the Invoke-WebRequest cmdlet
-Invoke-WebRequest -Uri $url -OutFile $filePath
+switch ($bitsJobObj.JobState) {
 
-# Install the executable file using the Start-Process cmdlet
-Start-Process -FilePath $filePath -ArgumentList -Wait
+    'Transferred' {
+        Complete-BitsTransfer -BitsJob $bitsJobObj
+        break
+    }
+
+    'Error' {
+        throw 'Error downloading'
+    }
+}
+
+$exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
+
+Start-Process -Wait $Destination -ArgumentList $exeArgs
