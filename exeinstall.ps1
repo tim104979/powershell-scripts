840a6d5f4a1240be6373c73814df50b30b3cdeff
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-09 09:06:35 +0200

    Update exeinstall.ps1

--- exeinstall.ps1
+++ exeinstall.ps1
@@ -1,26 +1,40 @@
+# Define the URL of the Ninite executable file
 $FileUri = "https://ninite.com/chrome-winrar/ninite.exe"
-$Destination = "C:/File.exe"
 
+# Define the destination path for the file
+$Destination = "C:\Ninite.exe"
+
+# Download the file using BITS
 $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
+# Wait for the download to complete
+while ($bitsJobObj.JobState -eq "Transferring") {
+    Start-Sleep -Milliseconds 100
+}
+
+# Check the job state
 switch ($bitsJobObj.JobState) {
 
     'Transferred' {
+        # Complete the BITS transfer
         Complete-BitsTransfer -BitsJob $bitsJobObj
+
+        # Define the arguments for the Ninite installer
+        $exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
+
+        # Force 32-bit execution on 64-bit systems
+        $workingDir = if ([Environment]::Is64BitOperatingSystem) { "C:\Windows\SysWOW64" } else { $env:SystemRoot }
+
+        # Run the Ninite installer
+        Start-Process -Wait -WorkingDirectory $workingDir -FilePath $Destination -ArgumentList $exeArgs
+
+        # Remove the file after installation
+        Remove-Item -Path $Destination -Force
         break
     }
 
     'Error' {
+        # Throw an error if the download fails
         throw 'Error downloading'
     }
 }
-
-$exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
-
-# Force 32-bit execution on 64-bit systems
-$workingDir = if ([Environment]::Is64BitOperatingSystem) { "C:\Windows\SysWOW64" } else { $env:SystemRoot }
-
-Start-Process -Wait -WorkingDirectory $workingDir -FilePath $Destination -ArgumentList $exeArgs
-
-# Remove the file after installation
-Remove-Item -Path $Destination -Force
