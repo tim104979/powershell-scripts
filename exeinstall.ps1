becb8bce3a0b82f1e385c7e9e3f5b41c6cc601d4
tim104979 <73826327+tim104979@users.noreply.github.com>
2024-10-09 09:02:27 +0200

    Update exeinstall.ps1

--- exeinstall.ps1
+++ exeinstall.ps1
@@ -17,6 +17,10 @@ switch ($bitsJobObj.JobState) {
 
 $exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
 
-Start-Process -Wait $Destination -ArgumentList $exeArgs
+# Force 32-bit execution on 64-bit systems
+$workingDir = if ([Environment]::Is64BitOperatingSystem) { "C:\Windows\SysWOW64" } else { $env:SystemRoot }
 
+Start-Process -Wait -WorkingDirectory $workingDir -FilePath $Destination -ArgumentList $exeArgs
+
+# Remove the file after installation
 Remove-Item -Path $Destination -Force
